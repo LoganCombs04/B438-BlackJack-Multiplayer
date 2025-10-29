1c82b509dd3199cf3b7568766e9b6d25a3b63ba5
LoganCombs04 <logancombs04@icloud.com>
2025-10-29 16:19:38 -0400

    Added the hand scene, which will function as a way to store player's cards and track their combined value.

---
+++ Scripts/hand.gd
@@ -0,0 +1,30 @@
+# The deck essentially "holds" all cards, and will be removed when 
+
+extends Node
+
+var CardsInHand: int = 0
+var HandValue: int = 0
+var HandNumber: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	
+func Reset_Hand() -> void:
+	CardsInHand = 0
+	HandValue = 0
+	
+	var children = get_children()
+	for child in children:
+		child.queue_free()
+		
+func Get_Card(NewCard: card) -> void:
+	self.add_child(NewCard)
+	HandValue += NewCard.value
+	CardsInHand += 1
+
+func Get_Card_Count() -> int:
+	return CardsInHand
+	
+func Get_Value() -> int:
+	return HandValue
